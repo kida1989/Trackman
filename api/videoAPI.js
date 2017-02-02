345bcfd48b21923a77f4585eade8893949400467
Erick S. Reyes <ereyes1989@gmail.com>
2017-02-02 18:14:17 -0500

    Started implementation of videoAPI
    Testing failed, additional work necessary before this task is complete.
    Keep working on it!

---
+++ api/videoAPI.js
@@ -0,0 +1,16 @@
+var Video = require('../models/video.js');
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/TMBBPro');
+router.get("/getvideo",function (req,res) {
+  Video.find({'_id':'acdf9221-1ec8-40b4-9b55-01e76e2b64dd'},function(err,doc){
+  if(err) {return handleError(err);}
+  else {
+    res.status(200).json(doc)
+  }
+
+
+});
+
+});
