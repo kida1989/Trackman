0c0c1a7557d6ebdc8393db21c5434776ddbf4079
Erick S. Reyes <ereyes1989@gmail.com>
2017-02-08 14:38:41 -0500

    Updated GET videoAPI (all & by ID)
    Small updated to HTML. Additional work necessary to update video based
    on GUID submitted in form.

--- api/videoAPI.js
+++ api/videoAPI.js
@@ -3,8 +3,13 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-router.get("/getvideo",function (req,res) {
-  Video.find({'_id':'acdf9221-1ec8-40b4-9b55-01e76e2b64dd'},function(err,doc){
+// GET video for a specifc play ID //
+router.get("/getvideo/:id",function (req,res) {
+
+  var id = req.params.id;
+
+  console.log(id);
+  Video.find({'_id':id},function(err,doc){
   if(err) {return handleError(err);}
   else {
     res.status(200).json(doc)
@@ -12,7 +17,23 @@ router.get("/getvideo",function (req,res) {
 
 
 });
+});
+
+
+// GET video for ALL plays //
+router.get("/getvideo/",function (req,res) {
+
+
+  Video.find({},function(err,doc){
+  if(err) {return handleError(err);}
+  else {
+    res.status(200).json(doc)
+  }
+
 
 });
+});
+
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+ var id = '8089bb39-8fdd-40f4-82fd-4ccfb76590ca';
+ var URL = 'http://127.0.0.1:3000/api/getvideo/';
+ var videoURL = URL + id;
+
+  request(videoURL, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(JSON.parse(body)[0].Url)
+      res.render('index', { videoURL: JSON.parse(body)[0].Url });
+    }
+  })
+
+
 });
 
 module.exports = router;
